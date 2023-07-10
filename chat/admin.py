e043a1da046124f178e55c7034f8fdc8d04b30a9
MaximilianScheel <scheel.development@gmail.com>
2023-07-10 14:35:48 +0200

    Implemented Django and created first db

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Message
 
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'author', 'receiver')
+    list_display = ('text', 'created_at', 'author', 'receiver') 
+    search_fields = ('text', 'author__username', 'receiver__username')
+    
+    
+    
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
