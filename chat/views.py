3ef1a2d274e067cfda0635c861d93d0d3b3c1260
MaximilianScheel <scheel.development@gmail.com>
2023-07-11 16:15:04 +0200

    Adding new class to db

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Message
 
 def index(request):
+    if request.method == 'POST':
+        print("Received data " + request.POST['textmessage'])
+        Message.objects.create(text=request.POST['textmessage'], chat=None, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html', {'username': 'Max'} )
