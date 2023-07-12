d38ab652bbbc45e49e6ef9607ca950534ebfc3c6
MaximilianScheel <scheel.development@gmail.com>
2023-07-12 14:25:12 +0200

    Implementing Login

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,26 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Message
+from .models import Chat, Message
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+
 
 def index(request):
+
     if request.method == 'POST':
         print("Received data " + request.POST['textmessage'])
-        Message.objects.create(text=request.POST['textmessage'], chat=None, author=request.user, receiver=request.user)
-    return render(request, 'chat/index.html', {'username': 'Max'} )
+        myChat = Chat.objects.get(id=1)
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        if user: 
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            return HttpResponseRedirect('/login/', { 'wrongPassword': True })
+    return render(request, 'auth/login.html')
